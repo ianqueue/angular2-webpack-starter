3f35bd9a2326a53d685e3a7258ee2808f312db49
Ian Quattlebaum <ian@qrift.com>
2016-03-22 14:23:29 -0400

    installing sass

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -170,6 +170,9 @@ module.exports = {
       // See: https://github.com/webpack/raw-loader
       {test: /\.html$/, loader: 'raw-loader', exclude: [helpers.root('src/index.html')]},
 
+      // sass loader
+      {test: /\.scss$/, exclude: /node_modules/, loaders: ['raw-loader', 'sass-loader']}
+
     ]
 
   },
